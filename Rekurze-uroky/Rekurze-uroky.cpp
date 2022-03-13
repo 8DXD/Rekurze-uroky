a6442e47e69448c5570d3409f19c9d1aa12606ec
Kateřina Dvořáková <kacule05@gmail.com>
2022-03-13 18:45:38 +0100

    Přidejte soubory projektu.

---
+++ Rekurze-uroky/Rekurze-uroky.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+//Úročení po dnech (složený úrok)
+long urok(float x) {
+	if (x < 10000) {
+		return 0;
+	}
+	else {
+		if (x <= 1) {
+			return x * 1.15;
+		}
+		else {
+			return urok(x - 1) * 1.15;
+		}
+	}
+	
+
+}
+
+int main()
+{
+	printf("Slozene uroceni--------------------------- \n Castka - 10000Kc\n Urok - 15 \n");
+	for (int j = 0; j < 6; j++) {
+		float a = 10000;
+		printf("%2d.rok %2f Kc\n", j, urok(a));
+	}
+}
